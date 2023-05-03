9a998132626a957e29af03a6d1a3ad3c50b1587b
Flávio Arêas <vflavio9@gmail.com>
2023-05-03 20:15:26 -0300

    feat: animated accordion

--- App.tsx
+++ App.tsx
@@ -1,21 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
 
 import { Accordion } from './src/components/Accordion';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <>
       <StatusBar style="auto" />
       <Accordion />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-  },
-});
--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,12 +1,28 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
+import {
+  Transition,
+  Transitioning,
+  TransitioningView,
+} from 'react-native-reanimated';
 import { data } from '../data';
+
+const transition = (
+  <Transition.Together>
+    <Transition.In type="fade" durationMs={200} />
+    <Transition.Change />
+    <Transition.Out type="fade" durationMs={200} />
+  </Transition.Together>
+);
 
 export function Accordion() {
   const [currentCardIndex, setCurrentCardIndex] = useState(-1);
+  const viewRef = useRef<TransitioningView>(null);
 
   function handleToggleCard(cardIndex: number) {
+    viewRef.current?.animateNextTransition();
+
     if (cardIndex === currentCardIndex) {
       setCurrentCardIndex(-1);
     } else {
@@ -15,14 +31,18 @@ export function Accordion() {
   }
 
   return (
-    <>
+    <Transitioning.View
+      ref={viewRef}
+      transition={transition}
+      style={styles.container}
+    >
       {data.map(
         ({ backgroundColor, category, color, subCategories }, index) => (
           <TouchableOpacity
             key={category}
             onPress={() => handleToggleCard(index)}
             activeOpacity={0.9}
-            style={styles.container}
+            style={styles.cardContainer}
           >
             <View style={[styles.card, { backgroundColor }]}>
               <Text style={[styles.heading, { color }]}>{category}</Text>
@@ -42,12 +62,17 @@ export function Accordion() {
           </TouchableOpacity>
         )
       )}
-    </>
+    </Transitioning.View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+  },
+  cardContainer: {
     flexGrow: 1,
   },
   card: {
